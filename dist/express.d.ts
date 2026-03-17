bdbe2af0383747689d5f01de4176fb3130cc6208
Mohd Sadiq <mohd.sadiq@dotevolve.co.in>
2026-03-17 23:28:53 +0530

    feat: add explicit ./express export alongside ./node and ./react

---
+++ dist/express.d.ts
@@ -0,0 +1,12 @@
+export { AppError, ValidationError, AuthenticationError, AuthorizationError, NotFoundError, ConflictError, } from "./errors/AppError";
+export { ErrorCategory } from "./errors/ErrorCategory";
+export type { ErrorCategoryType } from "./errors/ErrorCategory";
+export { correlationIdMiddleware } from "./middleware/correlationId";
+export { errorHandlerMiddleware, setupSentryErrorHandler, } from "./middleware/errorHandler";
+export type { ErrorResponse } from "./middleware/errorHandler";
+export { setupSentryMiddleware, attachSentryContext, } from "./middleware/sentryMiddleware";
+export { initializeSentry } from "./sentry/config";
+export type { SentryConfig } from "./sentry/config";
+export { withTransaction } from "./db/transactionHandler";
+export { asyncHandler } from "./utils/asyncHandler";
+export { sanitizeData, sanitizeUrl } from "./utils/sanitizer";
